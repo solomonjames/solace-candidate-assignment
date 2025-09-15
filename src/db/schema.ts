43b7f96c4097292c4409806e8b2f6183aa5048b1
James Solomon <solomonjames@gmail.com>
2025-09-15 12:31:48 -0400

    [db]: Updates to the schema data types

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,24 +1,22 @@
-import { sql } from "drizzle-orm";
 import {
   pgTable,
-  integer,
-  text,
   jsonb,
   serial,
   timestamp,
-  bigint,
+  smallint,
+  varchar,
 } from "drizzle-orm/pg-core";
 
 const advocates = pgTable("advocates", {
   id: serial("id").primaryKey(),
-  firstName: text("first_name").notNull(),
-  lastName: text("last_name").notNull(),
-  city: text("city").notNull(),
-  degree: text("degree").notNull(),
-  specialties: jsonb("payload").default([]).notNull(),
-  yearsOfExperience: integer("years_of_experience").notNull(),
-  phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
+  firstName: varchar("first_name").notNull(),
+  lastName: varchar("last_name").notNull(),
+  city: varchar("city").notNull(),
+  degree: varchar("degree", { length: 100 }).notNull(),
+  specialties: jsonb("specialties").default([]).notNull(),
+  yearsOfExperience: smallint("years_of_experience").notNull(),
+  phoneNumber: varchar("phone_number").notNull(),
+  createdAt: timestamp("created_at").defaultNow()
 });
 
 export { advocates };
