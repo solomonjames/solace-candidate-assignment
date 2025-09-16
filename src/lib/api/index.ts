70147e1ed118dd9548aa295edaa587c7ba43c54d
James Solomon <solomonjames@gmail.com>
2025-09-16 10:53:15 -0400

    [api]: Needed to make the env public for the client

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,10 +1,10 @@
 import { AdvocateApi } from '@/lib/api/advocate-api';
 import { ApiClient } from '@/lib/api/api-client';
 
-if (!process.env.API_URL) throw new Error('API_URL must be provided');
+if (!process.env.NEXT_PUBLIC_API_URL) throw new Error('API_URL must be provided');
 
 const apiClient = new ApiClient({
-  baseUrl: process.env.API_URL,
+  baseUrl: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json'
   },
